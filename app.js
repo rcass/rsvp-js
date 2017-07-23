47775ed4e377ee77f0b178cf4e5e3ceefc17bf08
Rose Cass <rose.cass12@gmail.com>
2017-07-23 11:32:17 -0700

    Started filter functionality

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 const form = document.getElementById('registrar');
 const input = document.querySelector('input');
+
+const mainDiv = document.querySelector('.main');
 const ul = document.getElementById('invitedList');
+
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckBox = document.createElement('input');
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckBox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox);
+mainDiv.insertBefore(div, ul);
+filterCheckBox.addEventListener('change', (e) => {
+  const isChecked = e.target.checked;
+  const lis = ul.children;
+  if (isChecked){
+    for (let i = 0; i < lis.length; i++){
+      let li = lis[i];
+      if(li.className === 'responded'){
+        li.style.display = '';
+      } else {
+        li.style.display = 'none';
+      }
+    }
+  } else {
+    for (let i = 0; i < lis.length; i++){
+      let li = lis[i];
+    }
+  }
+
+});
 
 function createLi(text){
   // create new name in list
